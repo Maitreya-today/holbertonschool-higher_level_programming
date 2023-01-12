c1fcc261b9ca7d67212dff210e19751cfc600d8f
Maitreya-today <lessismoreclint@gmail.com>
2023-01-12 05:39:57 +0000

    complete redo on code for fizzbuzz using terminal and vim

--- 0x01-python-if_else_loops_functions/12-fizzbuzz.py
+++ 0x01-python-if_else_loops_functions/12-fizzbuzz.py
@@ -3,9 +3,9 @@ def fizzbuzz():
     for x in range(1, 101):
         if x % 3 == 0 and x % 5 == 0:
             print("FizzBuzz", end=" ")
-        elif x % 3 is 0:
+        elif x % 3 == 0:
             print("Fizz", end=" ")
-        elif x % 5 is 0:
+        elif x % 5 == 0:
             print("Buzz", end=" ")
         else:
             print(x, end=" ")
