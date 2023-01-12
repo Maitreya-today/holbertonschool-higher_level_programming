e1ec676236816295839c1451fe6abf38092931e0
Maitreya-today <110085807+Maitreya-today@users.noreply.github.com>
2023-01-11 23:32:46 -0600

    update code on fizzbuzz

--- 0x01-python-if_else_loops_functions/12-fizzbuzz.py
+++ 0x01-python-if_else_loops_functions/12-fizzbuzz.py
@@ -1,11 +1,11 @@
 #!/usr/bin/python3
 def fizzbuzz():
-    for num in range(1, 101):
-        if num x % 3 == 0 and x % 5 == 0:
+    for x in range(1, 101):
+        if x % 3 == 0 and x % 5 == 0:
             print("FizzBuzz", end=" ")
-        elif num % 3 is 0:
+        elif x % 3 is 0:
             print("Fizz", end=" ")
-        elif num % 5 is 0:
+        elif x % 5 is 0:
             print("Buzz", end=" ")
         else:
-            print(num, end=" ")
+            print(x, end=" ")
