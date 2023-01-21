66ad2aef543862d2366c4c1c4143b94132548568
Maitreya-today <lessismoreclint@gmail.com>
2023-01-21 23:03:26 +0000

    corrected loop type on line 10 of 1-square.py file

--- 0x04-python-classes/1-square.py
+++ 0x04-python-classes/1-square.py
@@ -2,11 +2,11 @@
 """Square class is square"""
 
 
-class square:
+class Square:
     """it's a square with a size"""
     def __init__(self, size=0):
         if not isinstance(size, int):
-            raise TypeError("size must be integer")
-        if size < 0:
+            raise TypeError("size must be an integer")
+        elif size < 0:
             raise ValueError("size must be >= 0")
         self.__size = size
