aab76c44d3f5d3b53ecf6aac23ea4bea146d7f2f
Maitreya-today <110085807+Maitreya-today@users.noreply.github.com>
2023-01-21 16:03:31 -0600

    vscode create and push for task 1 - test

---
+++ 0x04-python-classes/1-square.py
@@ -0,0 +1,12 @@
+#!/usr/bin/python3
+"""Square class is square"""
+
+
+class square:
+    """it's a square with a size"""
+    def __init__(self, size=0):
+        if not isinstance(size, int):
+            raise TypeError("size must be integer")
+        elif size < 0:
+            raise ValueError("size must be >=0")
+        self.__size = size
