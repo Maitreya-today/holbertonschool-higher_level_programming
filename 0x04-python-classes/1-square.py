7f049378f38883d73be76d55d0f617c50e360b2d
Maitreya-today <lessismoreclint@gmail.com>
2023-01-21 22:21:30 +0000

    made spacing corrections in code and push from VIM

--- 0x04-python-classes/1-square.py
+++ 0x04-python-classes/1-square.py
@@ -7,6 +7,6 @@ class square:
     def __init__(self, size=0):
         if not isinstance(size, int):
             raise TypeError("size must be integer")
-        elif size < 0:
-            raise ValueError("size must be >=0")
+        if size < 0:
+            raise ValueError("size must be >= 0")
         self.__size = size
