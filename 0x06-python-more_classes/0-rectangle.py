12b55b2f8ff5ada20a7d26e2e5ac10fb66280565
Maitreya-today <lessismoreclint@gmail.com>
2023-01-23 05:36:10 +0000

    i see you

--- 0x06-python-more_classes/0-rectangle.py
+++ 0x06-python-more_classes/0-rectangle.py
@@ -1,17 +1,13 @@
 #!/usr/bin/python3
-"""We make rectangle"""
-
 
 class Rectangle:
-    """Rectangle have size of width and height
-    """
     def __init__(self, width=0, height=0):
-        self.__width = width
-        self.__height = height
+        self.width = width
+        self.height = height
 
     @property
     def width(self):
-        return self.__width
+        return self._width
 
     @width.setter
     def width(self, value):
@@ -19,11 +15,11 @@ class Rectangle:
             raise TypeError("width must be an integer")
         if value < 0:
             raise ValueError("width must be >= 0")
-        self.__width = value
+        self._width = value
 
     @property
     def height(self):
-        return self.__height
+        return self._height
 
     @height.setter
     def height(self, value):
@@ -31,4 +27,4 @@ class Rectangle:
             raise TypeError("height must be an integer")
         if value < 0:
             raise ValueError("height must be >= 0")
-        self.__height = value
+        self._height = value
