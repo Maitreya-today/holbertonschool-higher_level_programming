32c5e7272d734bcf1380347817fe2cf97823f9b0
Maitreya-today <110085807+Maitreya-today@users.noreply.github.com>
2023-01-22 23:53:03 -0600

    added comments and ran inside VSCode - push VSC

--- 0x06-python-more_classes/0-rectangle.py
+++ 0x06-python-more_classes/0-rectangle.py
@@ -1,17 +1,20 @@
 #!/usr/bin/python3
-
+"""0-rectangle.py"""
 
 class Rectangle:
+    """Defines a rectangle"""
     def __init__(self, width=0, height=0):
         self.width = width
         self.height = height
 
     @property
     def width(self):
+        """Gets width"""
         return self._width
 
     @width.setter
     def width(self, value):
+        """Sets width"""
         if not isinstance(value, int):
             raise TypeError("width must be an integer")
         if value < 0:
@@ -20,10 +23,12 @@ class Rectangle:
 
     @property
     def height(self):
+        """Gets Height"""
         return self._height
 
     @height.setter
     def height(self, value):
+        """Sets height"""
         if not isinstance(value, int):
             raise TypeError("height must be an integer")
         if value < 0:
