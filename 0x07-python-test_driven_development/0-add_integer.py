6c7e5d5038f03cd71e826e7cd71ba40d3c446253
Maitreya-today <lessismoreclint@gmail.com>
2023-01-25 01:57:46 +0000

    either reset up VIM or learn VSC now, time is ticking

--- 0x07-python-test_driven_development/0-add_integer.py
+++ 0x07-python-test_driven_development/0-add_integer.py
@@ -1,17 +1,10 @@
 #!/usr/bin/python3
-"""module comment
-"""
+"""Add integers"""
 
 
 def add_integer(a, b=98):
-    """"Adding two intergers"""
+    """"Returns sum of a and b""""
 
-    if not isinstance(a, int) and not isinstance(a, float):
-        raise TypeError("a must be an integer")
-    if not isinstance(b, int) and not isinstance(b, float):
-        raise TypeError("b must be an integer")
-    if isinstance(a, float):
-        a = int(a)
-    if isinstance(b, float):
-        b = int(b)
-    return (a + b)
+	if type(a) != int and type(a) != float:
+		raise TypeError("a must be an integer")
+	
