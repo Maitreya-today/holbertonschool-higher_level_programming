48cd007f9c32ad19013d54434d6941e1a08f3989
Maitreya-today <lessismoreclint@gmail.com>
2023-01-29 23:18:30 +0000

    what did i just screw up?

--- 0x0B-python-input_output/0-read_file.py
+++
@@ -1,9 +0,0 @@
-#!/usr/bin/python3
-"""Read text file (UTF8)"""
-
-
-def read_file(filename=""):
-    """reads a text file (UTF8) and prints it to stdout"""
-    with open(filename, "r", encoding="utf-8") as f:
-        for text in f:
-            print(text, end="")
