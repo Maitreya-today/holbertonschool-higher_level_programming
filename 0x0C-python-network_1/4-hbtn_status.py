59adad6f1ed5a1676cc56875cf88e2064e333308
Maitreya-today <lessismoreclint@gmail.com>
2023-03-10 02:03:44 -0800

    task 4 revision

--- 0x0C-python-network_1/4-hbtn_status.py
+++ 0x0C-python-network_1/4-hbtn_status.py
@@ -1,14 +1,13 @@
 #!/usr/bin/python3
 """
-    Fetches https://intranet.hbtn.io/status
+Write a Python script that fetches
+https://intranet.hbtn.io/status
 """
-
 import requests
 
-if __name__ == "__main__":
-    url = 'https://intranet.hbtn.io/status'
-    response = requests.get(url)
 
-    print("Body response:")
-    print("\t- type: {}".format(type(response.text)))
-    print("\t- content: {}".format(response.text))
+if __name__ == "__main__":
+    req = requests.get('https://intranet.hbtn.io/status')
+    print('Body response:')
+    print('\t- type: {}'.format(type(req.text)))
+    print('\t- content: {}'.format(req.text))
