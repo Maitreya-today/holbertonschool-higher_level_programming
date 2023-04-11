e1c1890373706874c4e606960d4856a79d88d57c
Maitreya-today <lessismoreclint@gmail.com>
2023-04-11 03:25:34 -0500

    heres nothing

---
+++ 0x01-javascript_objects_scopes_closures/6-square.js
@@ -0,0 +1,16 @@
+#!/usr/bin/node
+const Square = require('./5-square');
+
+class SquareWithCharPrint extends Square {
+  charPrint (c) {
+    if (c === undefined) {
+      c = 'X';
+    }
+    const row = c.repeat(this.width);
+    for (let i = 0; i < this.height; i++) {
+      console.log(row);
+    }
+  }
+}
+
+module.exports = SquareWithCharPrint;
